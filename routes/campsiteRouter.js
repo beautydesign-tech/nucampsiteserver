b9406a49d0fead729e98ec35b1b3062d6e50d2df
alex j <beautydesign.technique@gmail.com>
2020-11-28 11:09:24 -0500

    Added Schema and Models for Partners and Promotions

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -17,7 +17,7 @@ campsiteRouter
   .post((req, res, next) => {
     Campsite.create(req.body)
       .then((campsite) => {
-        console.log("Campsite Created ", campsite);
+        console.log("Campsite Created: ", campsite);
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
         res.json(campsite);
