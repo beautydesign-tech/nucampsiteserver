f4136d1a7fce5f0aa05481d90b6d2ef4d1487538
alex j <beautydesign.technique@gmail.com>
2020-12-03 14:02:04 -0500

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -49,26 +51,23 @@ app.use(
   })
 );
 
+// The following 2 lines are specifically for sessions (rather than Web Tokens)
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // therefore, we want to add authentication here before the client accesses data from the server
 function auth(req, res, next) {
-  console.log(req.session);
-  // the line above was added with Express Sessions - if using cookieParser
-  //you would change the following if argument and two other lines -- see last commit for details
-  if (!req.session.user) {
+  console.log(req.user);
+
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
@@ -80,7 +79,7 @@ app.use("/campsites", campsiteRouter);
 app.use("/partners", partnerRouter);
 app.use("/promotions", promotionRouter);
 
-// catch 404 and forward to error handler 
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
